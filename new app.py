1622401df97fb63758244628d8c2196d7734fbf1
Jinah1224 <baeja@visang.com>
2025-04-17 11:54:18 +0900

    Update new app.py

--- new app.py
+++ new app.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 import time
 from datetime import timedelta
 
-st.set_page_config(page_title="📚 교과서 커뮤니티 분석기", layout="wide")
+st.set_page_config(page_title="📚 AI 기반 교과서 관련 동향 분석기", layout="wide")
 st.title("📚 카카오톡 분석 + 뉴스 수집 통합 앱")
 
 # 뉴스 관련 설정
